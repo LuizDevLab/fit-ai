f477ef6f2e24fda169c0404293a0d27ad674aea5
LuizDevLab <luizgthub@gmail.com>
2026-05-17 22:29:45 -0300

    remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import "dotenv/config"
 
-// Import the framework and instantiate it
 import Fastify from "fastify";
 const fastify = Fastify({
   logger: true,
 });
 
-// Declare a route
 fastify.get("/", async function handler(request, reply) {
   return { hello: "world" };
 });
 
-// Run the server!
 try {
   await fastify.listen({ port: Number(process.env.PORT) || 8080 });
 } catch (err) {
